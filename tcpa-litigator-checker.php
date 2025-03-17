ebbf098ab29ef8c6534c87c199a664882e4bafcf
Noman E Jawad <nomanzawad@gmail.com>
2025-03-17 13:20:46 +0600

    final version for publish

--- tcpa-litigator-checker.php
+++ tcpa-litigator-checker.php
@@ -1,17 +1,6 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @since             1.0.0
- * @package           TCPA_Litigator_Checker
- *
- * @wordpress-plugin
  * Plugin Name:       TCPA Litigator Checker
  * Description:       A plugin to check if a phone number is associated with a known TCPA litigator. Using the API from TCPA Litigator List.
  * Version:           1.2.1
@@ -23,16 +12,12 @@
  * Domain Path:       /languages
  */
 
-// Prevent direct access to the file
 if (!defined('WPINC')) {
 	die;
 }
 
-/**
- * Define Plugin Constants
- */
-$plugin_data = get_file_data(__FILE__, ['Version' => 'Version']);
-define('TCPA_LITIGATOR_CHECKER_VERSION', $plugin_data['Version']);
+// Define Plugin Constants
+define('TCPA_LITIGATOR_CHECKER_VERSION', '1.2.1'); // Manually updated version
 define('TCPA_LITIGATOR_CHECKER_DIR', plugin_dir_path(__FILE__));
 define('TCPA_LITIGATOR_CHECKER_URL', plugin_dir_url(__FILE__));
 
@@ -64,8 +49,6 @@ require_once TCPA_LITIGATOR_CHECKER_DIR . 'includes/class-tcpa-litigator-checker
 
 /**
  * Add Settings link to the plugin page
- *
- * @since 1.0.0
  */
 function tcpa_litigator_checker_add_settings_link($links)
 {
@@ -78,12 +61,6 @@ add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'tcpa_litigator_c
 
 /**
  * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
  */
 function run_tcpa_litigator_checker()
 {
